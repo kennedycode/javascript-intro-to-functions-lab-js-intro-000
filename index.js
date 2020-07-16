743744795da0afd62cee393e9637d0b8c1a3f6ff
Andrew Kennedy <akennedycode@gmail.com>
2020-07-16 20:24:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function shout(string) {
   return string.toUpperCase()
 }
+function whisper(string) {
+  return string.toLowerCase()
+}
+function shout(string) {
+  console.log (string.toUpperCase())
+}
+function whisper(string) {
+  console.log(string.toLowerCase())
+}
